4d24c9533ff0878e3c62c8e99d56c6cf46844bbb
Francesco Torchia <francesco.torchia@studenti.unicam.it>
2026-01-21 12:30:49 +0100

    Aggiunta struttura backend Spring Boot

---
+++ backend/collectionmanager/src/main/java/com/francescotorchia/collectionmanager/CollectionmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.francescotorchia.collectionmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionmanagerApplication.class, args);
+	}
+
+}
---
+++ backend/collectionmanager/src/test/java/com/francescotorchia/collectionmanager/CollectionmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francescotorchia.collectionmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
